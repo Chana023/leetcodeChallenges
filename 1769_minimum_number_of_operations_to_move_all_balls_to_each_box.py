7e566b19ea3bbdff6b1a3fee437bc83994e02430
Chana023 <jesalchana23@gmail.com>
2025-04-10 19:37:42 +0200

    Completed 1769 minimum number of operations to move all balls to each box python challenge

---
+++ 1769_minimum_number_of_operations_to_move_all_balls_to_each_box.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/
+
+class Solution(object):
+    def minOperations(self, boxes):
+        
+
+            
+
+sol = Solution()
+sol.minOperations("110")
