8d3f0c345382e59e6c80de78f3132ed04839219a
Chana023 <jesalchana23@gmail.com>
2025-04-11 19:51:12 +0200

    Completed 1769 minimum number of operations to move all balls to each box

--- 1769_minimum_number_of_operations_to_move_all_balls_to_each_box.py
+++ 1769_minimum_number_of_operations_to_move_all_balls_to_each_box.py
@@ -3,8 +3,22 @@
 class Solution(object):
     def minOperations(self, boxes):
         
+        ball_loc = []
 
+        for i in range(len(boxes)):
+            if boxes[i] == '1':
+                ball_loc.append(i)
+
+        res = []
+        for i in range(len(boxes)):
+            opers = 0
+            for j in ball_loc:
+                opers = opers + abs(i - j)
+            res.append(opers)
+
+        print(res)
+        return res
             
 
 sol = Solution()
-sol.minOperations("110")
+sol.minOperations("001011")
